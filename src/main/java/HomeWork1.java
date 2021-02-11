13a6a1335668a59f042ac801d10d162b84a1e99b
Alexey <alexav5@yandex.ru>
2021-02-11 16:14:38 +0700

    add homework lesson 1

---
+++ src/main/java/HomeWork1.java
@@ -0,0 +1,60 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte a = 121;
+        short b = 31100;
+        int c = 2147483647;
+        long d = 2147483647L;
+        float e = 1.5f;
+        double f = 1.5;
+        char g = '@';
+        boolean isNaN = true;
+        System.out.println(isSumCheck(1, 7));
+        MinusAlert(-5);
+        MinusAlert(0);
+        MinusAlert(5);
+        System.out.println(isMinus(-5));
+        System.out.println(isMinus(0));
+        System.out.println(isMinus(5));
+        helloName("Леша");
+        leapYear(3700);
+
+    }
+
+    public static float action(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+
+    public static boolean isSumCheck(int a, int b) {
+        if ((a + b >= 10) && ((a + b <= 20))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void MinusAlert(int a) {
+        if (a < 0) {
+            System.out.println("Число отрицательное!");
+        } else {
+            System.out.println("Число положительное!");
+        }
+    }
+
+    public static boolean isMinus(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void helloName(String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+
+    public static void leapYear(int year) {
+        if ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0)) {
+            System.out.println("Високосный");
+        } else System.out.println("Не високосный");
+    }
+}
