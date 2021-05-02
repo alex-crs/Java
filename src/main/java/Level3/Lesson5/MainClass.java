3f3c5f9e334a05f2c2be5c30837bb0fc18abb3bd
Andreev Aleksey <alexav5@yandex.ru>
2021-05-03 00:01:19 +0300

    Added solution Lesson3_5

--- src/main/java/Level3/Lesson5/MainClass.java
+++ src/main/java/Level3/Lesson5/MainClass.java
@@ -1,18 +1,30 @@
 package Level3.Lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        CyclicBarrier start = new CyclicBarrier(CARS_COUNT + 1);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), start);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        while (true) {
+            if (start.getNumberWaiting() == CARS_COUNT) {
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+                start.await();
+                break;
+            }
+        }
+        start.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/main/java/Level3/Lesson5/Tunnel.java
+++ src/main/java/Level3/Lesson5/Tunnel.java
@@ -1,7 +1,11 @@
 package Level3.Lesson5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
-    public Tunnel() {
+    private Semaphore tunnel;
+    public Tunnel(int permits) {
+        this.tunnel = new Semaphore(permits/2);
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
@@ -10,12 +14,14 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                tunnel.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                tunnel.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
