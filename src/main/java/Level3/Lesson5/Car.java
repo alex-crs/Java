bf2f89e66de3267b3187134a53909810099c3190
Andreev Aleksey <alexav5@yandex.ru>
2021-05-03 00:01:19 +0300

    Added solution Lesson3_5

--- src/main/java/Level3/Lesson5/Car.java
+++ src/main/java/Level3/Lesson5/Car.java
@@ -1,36 +1,74 @@
 package Level3.Lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    private static boolean isWin;
+    private CyclicBarrier start;
+    private ReentrantReadWriteLock lock;
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier start) {
+        lock = new ReentrantReadWriteLock();
+        this.start = start;
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            start.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
+        }
+        setIsWin();
+        try {
+            start.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setIsWin() {
+        try {
+            lock.writeLock().lock();
+            if (!isWin) {
+                isWin = true;
+                System.out.println(this.name + " WIN");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.writeLock().unlock();
         }
     }
 }
